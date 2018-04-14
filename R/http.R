d9abfc56cc98c942719550cbda8b5722fde99e15
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-14 17:16:43 +0100

    cleanup http code & fix session_token arg

--- R/http.R
+++ R/http.R
@@ -2,12 +2,13 @@
 #' @description This is the workhorse function to execute calls to the Translate API.
 #' @param action A character string specifying the API action to take
 #' @param query An optional named list containing query string parameters and their character values.
+#' @param headers A list of headers to pass to the HTTP request.
 #' @param body A request body
 #' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
-#' @param region A character string containing an AWS region. If missing, the default \dQuote{us-east-1} is used.
-#' @param key A character string containing an AWS Access Key ID. The default is pulled from environment variable \dQuote{AWS_ACCESS_KEY_ID}.
-#' @param secret A character string containing an AWS Secret Access Key. The default is pulled from environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
-#' @param session_token Optionally, a character string containing an AWS temporary Session Token. If missing, defaults to value stored in environment variable \dQuote{AWS_SESSION_TOKEN}.
+#' @param region A character string specifying an AWS region. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param key A character string specifying an AWS Access Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param secret A character string specifying an AWS Secret Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param session_token Optionally, a character string specifying an AWS temporary Session Token to use in signing a request. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param ... Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return If successful, a named list. Otherwise, a data structure of class \dQuote{aws-error} containing any error message(s) from AWS and information about the request attempt.
 #' @details This function constructs and signs an Translate API request and returns the results thereof, or relevant debugging information in the case of error.
@@ -17,19 +18,27 @@
 #' @importFrom aws.signature signature_v4_auth
 #' @export
 translateHTTP <- 
-function(action,
-         query = list(),
-         body = NULL,
-         verbose = getOption("verbose", FALSE),
-         region = NULL, 
-         key = NULL, 
-         secret = NULL, 
-         session_token = NULL,
-         ...) {
+function(
+  action,
+  query = list(),
+  headers = list(),
+  body = NULL,
+  verbose = getOption("verbose", FALSE),
+  region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
+  key = NULL, 
+  secret = NULL, 
+  session_token = NULL,
+  ...
+) {
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # generate request signature
     d_timestamp <- format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "UTC")
-    if (is.null(region) || region == "") {
-        region <- "us-east-1"
-    }
     url <- paste0("https://translate.",region,".amazonaws.com")
     Sig <- signature_v4_auth(
            datetime = d_timestamp,
@@ -47,17 +56,19 @@ function(action,
            secret = secret,
            session_token = session_token,
            verbose = verbose)
-    headers <- list(host = paste0("translate.",region,".amazonaws.com"),
-                    "X-Amz-Date" = d_timestamp,
-                    "X-Amz-Target" = paste0("AWSShineFrontendService_20170701.", action),
-                    "Content-Type" = "application/x-amz-json-1.1")
+    # setup request headers
+    headers[["host"]] <- paste0("translate.",region,".amazonaws.com")
+    headers[["X-Amz-Target"]] <- paste0("AWSShineFrontendService_20170701.", action)
+    headers[["Content-Type"]] <- "application/x-amz-json-1.1"
+    headers[["x-amz-date"]] <- d_timestamp
     headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     if (!is.null(session_token) && session_token != "") {
         headers[["x-amz-security-token"]] <- session_token
     }
-    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     H <- do.call(add_headers, headers)
-        
+    
+    # execute request
     if (length(query)) {
         r <- POST(url, H, query = query, body = body, encode = "json", ...)
     } else {
